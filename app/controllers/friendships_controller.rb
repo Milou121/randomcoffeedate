05e41c5a7e39a36f0e6487e10b956ac2604357c5
Jay Yoon <jay.yoon7@gmail.com>
2016-06-15 17:35:39 +0200

    update friends status on dashboard all fixed

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,6 +11,7 @@ class FriendshipsController < ApplicationController
 
     def create
       invitee = User.find_by_email(params[:user][:address])
+
       if current_user.invite invitee
         redirect_to new_friend_path, :notice => "Successfully invited friend!"
       else
@@ -19,11 +20,12 @@ class FriendshipsController < ApplicationController
     end
 
     def update
-      inviter = User.find_by_email(params[:user][:address])
+      inviter = User.find(params[:id])
+
       if current_user.approve inviter
-        redirect_to new_friend_path, :notice => "Successfully confirmed friend!"
+        redirect_to dashboard_path
       else
-        redirect_to new_friend_path, :notice => "Sorry! Could not confirm friend!"
+        redirect_to dashboard_path
       end
     end
 
@@ -35,16 +37,14 @@ class FriendshipsController < ApplicationController
       @pending_invites = current_user.pending_invited
     end
 
-    def approve
 
-    end
 
     def destroy
       user = User.find_by_id(params[:id])
       if current_user.remove_friendship user
-        redirect_to friends_path, :notice => "Successfully removed friend!"
+        redirect_to dashboard_path, :notice => "Successfully removed friend!"
       else
-        redirect_to friends_path, :notice => "Sorry, couldn't remove friend!"
+        redirect_to dashboard_path, :notice => "Sorry, couldn't remove friend!"
       end
     end
 
