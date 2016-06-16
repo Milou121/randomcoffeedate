e61ab338a0e6a329b8ed8f8c857532cc9e6779e7
Jay Yoon <jay.yoon7@gmail.com>
2016-06-16 13:54:14 +0200

    add lang long to Location table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615131350) do
+ActiveRecord::Schema.define(version: 20160616115210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20160615131350) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "pot_friends", force: :cascade do |t|
