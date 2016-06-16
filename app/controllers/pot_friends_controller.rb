8b9e6c50b8da8e328e610974cbdf2b2276b45b2d
Milou Schelling <milou.schelling@gmail.com>
2016-06-16 11:31:55 +0200

    add pot friends controller

---
+++ app/controllers/pot_friends_controller.rb
@@ -0,0 +1,18 @@
+class PotFriendsController < ApplicationController
+
+  def new
+    @pot_friend = PotFriend.new
+  end
+
+  def create
+    @pot_friend = PotFriend.new(pot_friend_params)
+    @pot_friend.save!
+    redirect_to pot_friend_path(@pot_friend)
+  end
+
+  private
+
+  def pot_friend_params
+    params.require(:pot_friend).permit(:pot_id, :friend_id)
+  end
+end
---
+++ app/helpers/pot_friends_helper.rb
@@ -0,0 +1,2 @@
+module PotFriendsHelper
+end
