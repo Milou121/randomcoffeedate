39bafbc1ee5e81a9b9e755f99e8cb0af84f8a4ab
Jay Yoon <jay.yoon7@gmail.com>
2016-06-21 17:14:44 +0200

    update mailer to invite invitee who is not a cuppa user

--- app/mailers/friendship_mailer.rb
+++ app/mailers/friendship_mailer.rb
@@ -5,6 +5,19 @@
     mail(to: user.email, subject: 'You sent a friend request!')
     # This will render a view in `app/views/user_mailer`!
   end
+  def friends_request_without_account(user, invitee_email)
+    @user = user  # Instance variable => available in view
+    @invitee_email = invitee_email
+    mail(to: user.email, subject: "You sent a friend request to #{invitee_email}!")
+    # This
+  end
+
+  def friend_invite(user, invitee_email)
+    @user = user  # Instance variable => available in view
+    @invitee_email = invitee_email
+    mail(to: @invitee_email, subject: "#{@user.username} invited you for Cuppa!")
+    # This will render a view in `app/views/user_mailer`!
+  end
 
   def friends_request_invitee(user, invitee)
     @user = user  # Instance variable => available in view
