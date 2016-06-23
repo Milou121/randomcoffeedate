b3a174c19d7811d37b626c067afa5e0102bad819
Milou Schelling <milou.schelling@gmail.com>
2016-06-23 11:20:14 +0200

    fixed maps bug

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,5 +1,5 @@
 $(document).on('ready', function() {
-  $('a[href*=#]').on('click', function(e) {
+  $('a[href^="#"]').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
