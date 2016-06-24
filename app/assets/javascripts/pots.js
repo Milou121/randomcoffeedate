64d1ba2f25c4a3ad662f414fc89455873c83d725
Jay Yoon <jay.yoon7@gmail.com>
2016-06-24 12:03:56 +0200

    dashboard map icon

--- app/assets/javascripts/pots.js
+++ app/assets/javascripts/pots.js
@@ -4,10 +4,12 @@ $(document).on('ready', function() {
 
   $('#map').on('click', '.js-pot--select-location', function(e){
     e.preventDefault();
+
     var locationId = $(this).data('location-id');
     var locationName = $(this).data('location-name');
     $("#pot_location_id").val(locationId);
     $('.js-pot--selected-location').html(locationName);
   });
 });
+
 
