a3effaf7db8cb80c8c34d344ab5c64b3300fd9fa
Jay Yoon <jay.yoon7@gmail.com>
2016-06-24 16:01:38 +0200

    update relationship schema and features between cuppa match and canceled cuppa

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,15 @@ class DashboardController < ApplicationController
   def show
     @user = current_user
     @pots = current_user.pots.where(cup_id: nil)
-    @cups = current_user.pots.where.not(cup_id: nil).map(&:cup) # same as map { |pot| pot.cup }
+    @confirmed_cups = current_user.all_cups.where(status: "confirmed")
+    @canceled_cups = current_user.all_cups.where(status: "canceled")
+    # pots.where.not(cup_id: nil).map(&:cup) # same as map { |pot| pot.cup }
+
   end
+
+  # def cancel
+  #   @cup = current_user.pots.where.not(cup_id: nil).map(&:cup)
+  #   @cup.status = "canceled"
+  #   @cup.save!
+  # end
 end
