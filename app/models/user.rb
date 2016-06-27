836ba77b4990f8497c9639ee84e8e541885332ad
Jay Yoon <jay.yoon7@gmail.com>
2016-06-27 10:40:02 +0200

    validation for signup and pots#new

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,6 @@ class User < ActiveRecord::Base
 
   has_many :pots
 
-  # before_create :confirmation_token
-  # after_create :send_welcome_email
-  #
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
    :trackable, :validatable, :confirmable
 
@@ -15,14 +9,10 @@ class User < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
 
   validates :username, presence: true, uniqueness: true
+  validates :photo, presence: true
 
   def all_cups
     Cup.where("sender_id = :id OR receiver_id = :id", id: id)
   end
 
-  private
-
-  # def send_welcome_email
-  #   UserMailer.welcome(self).deliver_now
-  # end
 end
