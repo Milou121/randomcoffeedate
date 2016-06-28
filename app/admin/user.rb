9da553354de569181bf0ab093560b6bdddbce532
Milou Schelling <milou.schelling@gmail.com>
2016-06-28 10:26:16 +0200

    add helper to set time correctly in cup show

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -12,13 +12,13 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-# 
- index do 
-    selectable_column 
+#
+ index do
+    selectable_column
     column :id
     column :email
-    column :username 
-    column :admin 
+    column :username
+    column :admin
     column :last_sign_in_at
     actions
   end
