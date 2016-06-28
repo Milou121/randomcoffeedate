a0f4cca11a5d703a80a4f3e2ed160982b85a8fb7
Milou Schelling <milou.schelling@gmail.com>
2016-06-28 17:07:11 +0200

    work in progress on the bean picker

--- app/assets/javascripts/pots.js
+++ app/assets/javascripts/pots.js
@@ -22,6 +22,36 @@ $(document).on('ready', function() {
     }
   });
 
+  $(".js-pot--friends-picker").imagepicker({
+    selected: function (options) {
+      var faceHTML = options.node[0].innerHTML.split('src="')
+      // These return Cloudinary URL of correct face & bean
+      var faceSRC = faceHTML[1].slice(0, -8)
+      var beanSRC =  "http://res.cloudinary.com/dvj9whqch/image/upload/v1467032314/beanbw_i6lu5e.png"
+      console.log('options dog', options)
+
+      // returns an array of all images with class thumbnail (all friends)
+      // problem: can't grab correct friend img to set SRC back
+      // we have the bean SRC and correct face SRC
+      // trying to find a way to identify which div should be updated with face SRC
+      var image = $(".thumbnail img")
+      console.log('peking duck', image)
+      if(image.attr('src').indexOf('bean') != -1){
+        console.log('god no beans please', $(".thumbnail.selected img"))
+        // need to select the one that was clicked on
+
+        // we select the correct image by finding the correct
+        // element in the 'select' then using the index to replace
+        // the corresponding image
+        $(".thumbnail.selected img").attr("src", faceSRC)
+      } else {
+        console.log('bean me baby',$(".thumbnail img") )
+        // need to select the one that was clicked on
+        $(".thumbnail.selected img").attr("src", beanSRC)
+      }
+    }
+  });
+
   $('#map').on('click', '.js-pot--select-location', function(e){
     e.preventDefault();
 
