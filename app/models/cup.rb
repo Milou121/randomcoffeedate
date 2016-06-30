5524288af673ddbb0232403540f4550fb0f1be09
Milou Schelling <milou.schelling@gmail.com>
2016-06-30 13:55:12 +0200

    made dependencies work that it will destroy a pot and a cup if a user is destroyed

--- app/models/cup.rb
+++ app/models/cup.rb
@@ -5,5 +5,5 @@ class Cup < ActiveRecord::Base
   belongs_to :receiver, class_name: "User"
   belongs_to :canceled_by, class_name: "User"
 
-  has_many :pots
+  has_many :pots, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
-
-  has_many :pots
+  has_many :pots, dependent: :destroy
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
    :trackable, :validatable, :confirmable
 
   include Amistad::FriendModel
   mount_uploader :photo, PhotoUploader
+
+  has_many :sent_cups, dependent: :destroy, class_name: "Cup", foreign_key: "sender_id"
+  has_many :received_cups, dependent: :destroy, class_name: "Cup", foreign_key: "receiver_id"
 
   validates :username, presence: true, uniqueness: true
   validates :photo, presence: true
